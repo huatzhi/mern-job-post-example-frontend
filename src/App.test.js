07f2d8df47c6437e0d4774d9fdff8b91595034de
Huat Zhi <huatzhi@gmail.com>
2020-11-09 19:50:23 +0800

    test circleCI

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Recruiter/i);
   expect(linkElement).toBeInTheDocument();
 });
